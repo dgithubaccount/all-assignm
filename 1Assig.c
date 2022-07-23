3257e6cce51c98c1c8aaa92d9c4e33f5dfe50ad2
dgithubaccount <95975241+dgithubaccount@users.noreply.github.com>
2022-07-23 17:37:43 +0530

    Add files via upload

---
+++ 1Assig.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+int main()
+{
+    ////// 1 print hello on screen
+    /*
+    printf("Hello");
+    */
+
+
+    ///2  hello fisrt line Students in secod line
+    /*
+    printf(" Hello \n student");
+    */
+    // printf("student ");
+
+
+    /// mysirg in doule qouts
+    // printf("  mysirg ");
+
+
+    ///4  area of circle
+    /*
+    int a;
+    printf("Enter area of circle:");
+    scanf("%d %d",&a);
+    */
+
+
+
+    /// 5 length of string 
+    /*
+    int n = printf("Deepak \n");
+    printf("\n");
+    printf("length is : %d ",n);
+    */
+
+   /// 10 
+   /*
+   int d,m,y;
+   printf("Enter your date of Borth : \n");
+   printf("thsi formate (22/11/2022): \n");
+   scanf("%d %d %d",&d,&m,&y);
+   printf("Date - %d Month - %d Year - %d ",d,m,y);
+    */
+
+
+   /// 11
+   /*
+   int t,m;
+   printf("Enter the time : \n");
+   scanf("%d %d",&t, &m);
+   printf("%d hour and %d Minute .",t,m);
+   */
+
+
+  //// find output of code
+    int x = printf("ineuron");
+    printf("\n");
+    printf("Length is :%d ",x);    //7
+
+
+
+
+}
